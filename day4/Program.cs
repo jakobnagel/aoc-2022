b38fcdc2f05437f6cb9f9137db91be099179f405
Jakob-Nagel-Git <jakob1211@hotmail.com>
2022-12-04 01:28:09 -0500

    Creating C# project

---
+++ day4/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+
+string[] lines = File.ReadAllLines("input.txt");
+foreach (string line in lines)
+{
+  Console.WriteLine(line);
+}
+
+
+bool IsContained(string line)
+{
+
+}
